52ec82994f404e391e41a5346f5c4b22bc042b13
Camilo Ramirez <jcramirezhp@gmail.com>
2015-05-24 15:02:47 -0700

    Delete folder

--- IonicExperimentsTmp/www/app/push/push-ctrl.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('eliteApp').controller('PushCtrl', ['eliteApi', PushCtrl]);
-
-    function PushCtrl(eliteApi) {
-        var vm = this;
-    };
-})();
